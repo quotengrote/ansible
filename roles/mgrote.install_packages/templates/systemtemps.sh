295d3b59606ccaf26b5859b54f0424d7cb72397a
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-08-29 10:58:01 +0200

    Parameter watch-Scripte erweitert und ausgeschrieben

--- roles/mgrote.install_packages/templates/systemtemps.sh
+++ roles/mgrote.install_packages/templates/systemtemps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch -n 10 --color --no-title "ipmitool sensor | cut -d '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | sed -r 's/degrees C/°C/'"
+watch --interval 10 --differences --color --no-title "ipmitool sensor | cut -d '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | sed -r 's/degrees C/°C/'"
