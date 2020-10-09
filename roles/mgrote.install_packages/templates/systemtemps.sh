1cbdcbd0d6b801dc031b4fcf063bc657e5ccb71e
Quotengrote <38253905+quotengrote@users.noreply.github.com>
2020-10-09 20:11:34 +0200

    Scripte +  RegEx (#38)
    * Inventar K8s + Hosts hinzugefügt
    
    * wip
    
    * zfs-free - Spalten ausgeblendet
    
    * systemtemps Spalten + Design angepasst
    
    * watch entfernt aus systemtempsm quoting funktioniert nicht

--- roles/mgrote.install_packages/templates/systemtemps.sh
+++ roles/mgrote.install_packages/templates/systemtemps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch --interval 10 --differences --color --no-title "ipmitool sensor | cut -d '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | sed -r 's/degrees C/°C/'"
+sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete "|" | tr --squeeze-repeats " " | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\t\3\t\1/g'
