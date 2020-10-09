bfd9ac578ed839b93c1735e79eadffc300c4dc82
Quotengrote <38253905+quotengrote@users.noreply.github.com>
2020-10-09 20:11:53 +0200

    Revert "Scripte +  RegEx (#38)" (#39)
    This reverts commit 1cbdcbd0d6b801dc031b4fcf063bc657e5ccb71e.

--- roles/mgrote.zfs_tools/templates/zfs-free.sh
+++ roles/mgrote.zfs_tools/templates/zfs-free.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-watch --interval 60 --differences --color --no-title "sudo zfs list -o space | grep -v / | tr -s ' ' | cut --delimiter=' '  --fields=1-3 | sed -r 's/\s/\t\t/g;s/NAME/NAME\t/g'"
+watch --interval 60 --differences --color --no-title "zfs list -o space | grep -v /"
