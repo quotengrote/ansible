e43ef98d7c48326a7990e0f8407dc8aee10aba53
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-10-09 19:47:09 +0200

    watch entfernt aus systemtempsm quoting funktioniert nicht

--- roles/mgrote.install_packages/templates/systemtemps.sh
+++ roles/mgrote.install_packages/templates/systemtemps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch --interval 10 --differences --color --no-title "sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete "|" | tr --squeeze-repeats " " | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\t\3\t\1/g'"
+sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete "|" | tr --squeeze-repeats " " | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\t\3\t\1/g'
