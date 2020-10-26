c8ba352cec92a2b2ec13c22b28ca5ee27a800d62
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-10-26 09:26:58 +0100

    watch in systemtemps eingebunden(mit escaping geht es)

--- roles/mgrote.install_packages/templates/systemtemps.sh
+++ roles/mgrote.install_packages/templates/systemtemps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete "|" | tr --squeeze-repeats " " | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\3\t\1/g;s/(.000)//g'
+watch "sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete \"|\" | tr --squeeze-repeats \" \" | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\3\t\1/g;s/(.000)//g'"
