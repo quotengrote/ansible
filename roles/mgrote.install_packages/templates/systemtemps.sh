9a57dd913ee32f1826c6a55bb4215ac72dee7789
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-10-26 09:36:40 +0100

    Entferne "Temp" aus Ausgabe

--- roles/mgrote.install_packages/templates/systemtemps.sh
+++ roles/mgrote.install_packages/templates/systemtemps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch "sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete \"|\" | tr --squeeze-repeats \" \" | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\3\t\1/g;s/(.000)//g'"
+watch --interval 10 --differences --color --no-title  "sudo ipmitool sensor | cut --delimiter '|' -f 1-3 | grep -i "degrees" | sort -k2 -r | tr --delete \"|\" | tr --squeeze-repeats \" \" | sed -r 's/(degrees C)/°C/g;s/([[:alnum:]]+[[:space:]][[:alpha:]]+ )([[:digit:]]+\.[[:digit:]]+ )(°C)/\2\3\t\1/g;s/(.000)//g;s/( Temp)//g'"
