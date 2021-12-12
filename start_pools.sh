2f2595acdcdc4dca944b96ab624fec884af3c42e
mg <michael.grote@posteo.de>
2021-12-12 16:10:33 +0100

    Musterrolle + alte Scripte entfernt (#271)
    Co-authored-by: Michael Grote <michael.grote@posteo.de>
    Reviewed-on: https://git.mgrote.net/mg/ansible/pulls/271
    Co-authored-by: mg <michael.grote@posteo.de>
    Co-committed-by: mg <michael.grote@posteo.de>

--- start_pools.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh mg@pve2.grote.lan "sudo pvesh get /pools/$1 --output-format yaml | grep -E lxc/[0-9]{3} | sed -r 's/[[:blank:]a-zA-Z\/\:]+//g' | sudo xargs -i --verbose  pct $2 {} && sudo pvesh get /pools/$1 --output-format yaml | grep -E qemu/[0-9]{3} | sed -r 's/[[:blank:]a-zA-Z\/\:]+//g' | sudo xargs -i --verbose  qm $2 {}"
